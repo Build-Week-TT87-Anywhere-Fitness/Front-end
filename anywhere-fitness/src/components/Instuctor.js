658001bef3addcb34407ca6542ae69ba7d760053
XavierTwitty <x.twitty@gmail.com>
2021-01-29 16:07:08 -0500

    sign-up/ instuctor sign up form. updated code on membership signup form

--- anywhere-fitness/src/components/Instuctor.js
+++ anywhere-fitness/src/components/Instuctor.js
@@ -5,8 +5,8 @@ const formSchema = yup.object().shape({
   password: yup.string().required(" password is required "),
 });
 
-export default function Membership() {
-  const [memberForm, setMemberForm] = userState({
+export default function Instuctor() {
+  const [instuctorForm, setInstuctorForm] = userState({
     username: "",
     password: "",
   });
@@ -34,13 +34,13 @@ export default function Membership() {
   const inputchange = (e) => {
     e.persist();
     validate(e);
-    setMemberForm({ ...memberForm, [e.target.name]: e.target.value });
+    setInstuctorForm({ ...instuctorForm, [e.target.name]: e.target.value });
   };
 
   const submitForm = (e) => {
     e.preventDefault();
     axios
-      .post("https://reqres.in/api/membership", memberForm)
+      .post("https://reqres.in/api/membership", instuctorForm)
       .then((response) => console.log("form submitted", response))
       .catch((err) => console.log(err));
   };
@@ -54,9 +54,9 @@ export default function Membership() {
           id="username"
           name="username"
           onChange={inputchange}
-          value={memberForm.username}
+          value={instuctorForm.username}
         />
-        {errorState.username ? <p>{errorState.username}</p> : null}
+        {errorState.password ? <p>{errorState.username}</p> : null}
       </label>
       <label htmlFor="password">
         <input
@@ -64,7 +64,7 @@ export default function Membership() {
           id="passowrd"
           name="password"
           onChange={inputchange}
-          value={memberForm.password}
+          value={instuctorForm.password}
         />
         {errorState.password ? <p>{errorState.password}</p> : null}
       </label>
