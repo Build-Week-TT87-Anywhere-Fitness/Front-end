88748a0b3d01c54ddffc5a32c6c7af03c184f7b7
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-01-28 17:37:37 -0700

    working on signUp form

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 import './App.css';
 
@@ -12,7 +13,15 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <div className="nav-bar">
+            <nav>
+              <Link to="/Login">Login</Link>
+              <Link onClick={logout}>Logout</Link>
+              <Link to="/SignUp">Sign Up</Link>
+            </nav>
+          </div>
         <Route exact path="/" component={Login} />
+        <Route component={SignUp} />
             
       </div>
     </Router>
