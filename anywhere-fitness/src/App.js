63d24f1af69dc50ac6e0a027fb081c433e4eac77
XavierTwitty <x.twitty@gmail.com>
2021-02-01 14:59:42 -0500

    updated the member and instuctor login form and deleted the login file

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Login from "./components/Login";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import InstructorLogin from "./components/InstructorLogin";
+import MemberLogin from "./components/MemberLogin";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  const logout = () => {
-    localStorage.removeItem("token");
-  };
-
   return (
     <Router>
       <div className="App">
         <div className="nav-bar">
-            <nav>
-              <Link to="/Login">Login</Link>
-              <Link onClick={logout}>Logout</Link>
-              <Link to="/SignUp">Sign Up</Link>
-            </nav>
-          </div>
-        <Route exact path="/" component={Login} />
-        <Route component={SignUp} />
-            
+          <nav>
+            <Link to="/Home">Home</Link>
+            <Link to="/SignUp">Sign-Up</Link>
+            {/* <Link to="/Login">Login</Link> */}
+            <Link to="/InstructorLogin">Instructor</Link>
+            <Link to="/MemberLogin">Members</Link>
+          </nav>
+        </div>
+
+        <Switch>
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/SignUp" component={SignUp} />
+          {/* <Route exact path="/Login" component={Login} /> */}
+          <Route exact path="/InstructorLogin" component={InstructorLogin} />
+          <Route exact path="/MemberLogin" component={MemberLogin} />
+        </Switch>
       </div>
     </Router>
   );
